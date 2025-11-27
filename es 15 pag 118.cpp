7a5481863ac848f8b89ff85cef1d75eab7c9b486
GabrieleAmadesi <amadesi.gabriele.2023@itisgalileiroma.it>
2025-11-27 08:51:35 +0100

    Add files via upload

---
+++ es 15 pag 118.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+	{double, prezzo, prezzotot;}
+	int peso, kili;
+	cout << "inserisci il peso del bagaglio" << endl;
+	cin >> peso;
+	cout << "inserisci il costo del biglietto" << endl;
+	cin >> prezzo;
+	if (peso<=20); {cout << "il costo del tuo biglietto è:" << prezzo << endl;};
+	else if (peso>20); {kili = peso-20, percentuale=prezzo*0,02;};
+	prezzotot = percentuale*kili;
+	cout << "il prezzo totale del tuo biglietto è:" << prezzotot << endl;
+	
+	
+	
+	
+	
+	
+return 0;
+}
+
+
+
+	
+
