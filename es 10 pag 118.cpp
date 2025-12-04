c40ce3adccf7f0dbe1b1e1cdcca32e2c04b4e61a
GabrieleAmadesi <amadesi.gabriele.2023@itisgalileiroma.it>
2025-12-04 08:41:22 +0100

    Add files via upload

---
+++ es 10 pag 118.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{int età;
+ cout << "inserisci la tua età" << endl;
+cin >> età;
+if (età<14) {cout << "non puoi guidare nessun mezzo" << endl;}
+else if (età > 14)  {cout << "puoi guidare lo scooter con cilindrata 50cc" << endl;};
+else if (età > 16)  {cout << "puoi guidare la moto con cilindrata 125cc" << endl;};
+else if (età > 18)  {cout << "puoi guidare l'auto fino a 95 cavalli" << endl;};
+else if (età > 21)  {cout << "puoi guidare qualsiasi auto o moto senza limitazioni" << endl;};
+
+return 0;}
+
+
