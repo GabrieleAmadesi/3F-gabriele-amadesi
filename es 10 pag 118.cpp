7635b45ed0f0281754e283a80e0ef002c8c784e7
GabrieleAmadesi <amadesi.gabriele.2023@itisgalileiroma.it>
2025-12-04 08:44:53 +0100

    Fix formatting and structure of C++ code

--- es 10 pag 118.cpp
+++ es 10 pag 118.cpp
@@ -5,11 +5,12 @@ int main()
  cout << "inserisci la tua età" << endl;
 cin >> età;
 if (età<14) {cout << "non puoi guidare nessun mezzo" << endl;}
-else if (età > 14)  {cout << "puoi guidare lo scooter con cilindrata 50cc" << endl;};
-else if (età > 16)  {cout << "puoi guidare la moto con cilindrata 125cc" << endl;};
-else if (età > 18)  {cout << "puoi guidare l'auto fino a 95 cavalli" << endl;};
-else if (età > 21)  {cout << "puoi guidare qualsiasi auto o moto senza limitazioni" << endl;};
+else if (età > 14)  {cout << "puoi guidare lo scooter con cilindrata 50cc" << endl;}
+else if (età > 16)  {cout << "puoi guidare la moto con cilindrata 125cc" << endl;}
+else if (età > 18)  {cout << "puoi guidare l'auto fino a 95 cavalli" << endl;}
+else if (età > 21)  {cout << "puoi guidare qualsiasi auto o moto senza limitazioni" << endl;}
 
 return 0;}
+
 
 
